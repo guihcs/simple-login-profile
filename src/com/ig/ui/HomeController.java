1cadd9870709d67c77d7ea35fa7854eff81752a9
guilherme-fafic <guilherme.fafic@gmail.com>
2018-05-15 02:26:21 -0300

    Edit UI

--- src/com/ig/ui/HomeController.java
+++ src/com/ig/ui/HomeController.java
@@ -1,4 +1,17 @@
 package com.ig.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.web.WebView;
+
 public class HomeController {
+
+
+    @FXML
+    private WebView webView;
+
+    public void initialize(){
+        webView.getEngine().load("https://media1.tenor.com/images/5578616247515b540d877db81818f009/tenor.gif?itemid=7866344");
+    }
 }
