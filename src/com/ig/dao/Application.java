9dbddd1730c0bdad07d9f6a5e473392445909841
Iarly Medeiros <iarlypablo7@gmail.com>
2018-05-15 00:44:34 -0300

    Segunda Atualização com a implementação dos metodos o IO, Aplication, Dao e User

--- src/com/ig/dao/Application.java
+++ src/com/ig/dao/Application.java
@@ -1,4 +1,138 @@
 package com.ig.dao;
 
+import com.ig.io.IOFiles;
+import com.ig.model.User;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Application {
+
+    //Interface lista
+    private static List<User> bDUsers;
+    private static User userDefault;
+    private static HashMap<String, javafx.scene.Node> nodeHashMap;
+
+
+
+    //Construtor
+    public Application ()
+    {
+        bDUsers =  new LinkedList<>();
+    }
+
+    //Adicionar Usuários a lista
+    public boolean addUser(User user)
+    {
+        return (isContains(user)) ? bDUsers.add(user): false;
+    }
+
+    //Retorna verdadeiro caso o cpf exista, validando o login do usuário.
+    public static boolean login(String cpf)
+    {
+        for (User user : bDUsers)
+        {
+            if(cpf.equals(user.getCpf()))
+                userDefault = user;
+                return true;
+        }
+        return false;
+    }
+    //Retorna verdadeiro caso ja exista um usuário com esse CPF.
+    private static boolean isContains(User user){
+
+        for (User u : bDUsers)
+        {
+            if(u.getCpf().equals(user.getCpf()))
+                return true;
+        }
+        return false;
+    }
+
+    //Adiciona o nome no usuário Default
+    public Node getNode(String name){
+
+        userDefault.visitPagesAdd(name);
+        return nodeHashMap.get(name);
+
+    }
+
+    //Salva todos os ddos no arquivo
+    public void datasSaved(){
+        File dir =  new File("C:\\Users\\Iarly Medeiros\\Documents\\Usuarios");
+        dir.mkdirs();
+
+        for (User u: bDUsers)
+        {
+            File arq = new File("C:\\Users\\Iarly Medeiros\\Documents\\Usuarios\\"+u.getName()+".txt");
+
+            try
+            {
+                arq.createNewFile();
+                IOFiles.fileWriteAppend(arq,u.toString());
+                acessPagesSaved(u);
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    //Salva a lista de acesso em um arquivo com nome dos usuários
+    private void acessPagesSaved(User user){
+
+        File dir =  new File("C:\\Users\\Iarly Medeiros\\Documents\\Acessos");
+        dir.mkdirs();
+
+        List<String> list  = user.getAcessPages();
+
+        for (String acess:list) {
+            File arq = new File("C:\\Users\\Iarly Medeiros\\Documents\\Acessos\\"+user.getName()+".txt");
+            try {
+                arq.createNewFile();
+                IOFiles.fileWriteAppend(arq,acess);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    //Carrega todos os dados
+    public boolean loaderDatas(File userfile,File acessFile)
+    {
+        if(isEmpty(userfile) == 0)
+            return false;
+
+        User user;
+        List<String> userList =  IOFiles.fileReader(userfile);
+        List<String> acessList =  IOFiles.fileReader(acessFile);
+
+        for (int i = 0; i < userList.size() ; i++)
+        {
+            String uD[] = userList.get(i).split("#");
+            user = new User(new Image(uD[0]),uD[1],uD[2]);
+
+            for (String acess: acessList)
+            {
+                user.getAcessPages().add(acess);
+            }
+        }
+        return true;
+    }
+
+    //Verifica se o arquivo está vazio
+    private int isEmpty(File file)
+    {
+        return file.length() == 0 ? 0 : 1;
+    }
+
 }
--- src/com/ig/io/IOFiles.java
+++ src/com/ig/io/IOFiles.java
@@ -1,4 +1,49 @@
 package com.ig.io;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class IOFiles {
+
+    public static void fileWriteAppend(File file, String conteudo){
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true))){
+            bufferedWriter.write(conteudo);
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void fileWrite(File file, String conteudo){
+
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(conteudo);
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static List<String> fileReader(File file){
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
+            List<String> userLines = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()).equals(null)){
+                userLines.add(line);
+            }
+            return userLines;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
